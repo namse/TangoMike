8373e9ad6fa3968b61a5fdf43be62322366b230d
skatpgusskat <skatpgusskat@naver.com>
2014-08-18 00:29:22 +0900

    Add Component for parent-child render tree

---
+++ Src/TangoMike/TangoMike/Component.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "Component.h"
+
+
+Component::Component()
+{
+}
+
+
+Component::~Component()
+{
+}
+
+void Component::Update(float dTime)
+{
+
+}
+
+void Component::Render()
+{
+
+}
---
+++ Src/TangoMike/TangoMike/Component.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "stdafx.h"
+class Component
+{
+public:
+	Component();
+	virtual ~Component();
+
+	void AddChild(Component* child);
+	void RemoveChild(Component* child);
+
+	virtual void Update(float dTime);
+	virtual void Render();
+
+	// about Transforms : http://msdn.microsoft.com/en-us/library/windows/desktop/ff684172(v=vs.85).aspx
+	void SetPosition(D2D_VECTOR_2F position)	{ position_ = position; }
+	void SetCenter(D2D_VECTOR_2F center)		{ center_ = center; }
+	void SetRotation(float rotation)				{ rotation_ = rotation; }
+	void SetScale(D2D_VECTOR_2F scale)			{ scale_ = scale; }
+
+	D2D_VECTOR_2F GetPosition()					{ return position_; }
+	D2D_VECTOR_2F GetCenter()					{ return center_; }
+	float GetRotation()								{ return rotation_; }
+	D2D_VECTOR_2F GetScale()					{ return scale_; }
+
+
+private:
+	Component* parent_;
+	std::list<Component*> childs_;
+	
+	D2D_VECTOR_2F position_;
+	D2D_VECTOR_2F center_;
+	float rotation_;
+	D2D_VECTOR_2F scale_;
+
+};
+
